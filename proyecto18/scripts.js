9cf95484d271f4751d80937a224ddc9bb0d94bf9
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-07-27 22:48:55 -0400

    Proyecto 18 cronometro hecho

---
+++ proyecto18/scripts.js
@@ -0,0 +1,47 @@
+window.addEventListener("DOMContentLoaded", (evento) => {
+    //capturar botones
+    let botonIniciar = document.getElementById("boton-iniciar");
+    let botonParar = document.getElementById("boton-parar");
+    //capturar minutos y segundos
+    let segundosP = document.getElementById("segundos");
+    let minutosP = document.getElementById("minutos");
+    //funciones
+    let segundos = 0;
+    let minutos = 0;
+    let time = null;
+    let iniciar = () => {
+        if (!time) {
+            time = setInterval(() => {
+                segundos++;
+                if (segundos <= 9) {
+                    segundosP.textContent = `0${segundos} `;
+                }
+                else {
+                    segundosP.textContent = `${segundos} `;
+                }
+                if (segundos == 60) {
+                    segundos = 0;
+                    minutos++;
+                    segundosP.textContent = `${segundos}0 `;
+                    minutosP.textContent = `0${minutos}`;
+                }
+                if (minutos >= 60) {
+                    alert("Tiempo exedido");
+                    clearInterval(time);
+                }
+
+            }, 1000);
+        }
+    };
+    //Eventos de los botones
+    botonIniciar.addEventListener("click", () => {
+        iniciar();
+    });
+    botonParar.addEventListener("click", () => {
+        if (time){
+            clearInterval(time);
+            time = null;
+        }
+    });
+
+});
