bc3807fb02120a5d87c76cffb7f9ff2bc6287bcd
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-03 21:26:52 -0400

    primerso cambios en el proyecto 20 nuevo 3

--- proyecto 20/funciones.js
+++ proyecto 20/funciones.js
@@ -1,19 +1,29 @@
-window.addEventListener("DOMContentLoaded",()=>{
-    
+window.addEventListener("DOMContentLoaded", (event) => {
+    event.preventDefault();
     //tomar id de los HTML
-    let fecha= document.getElementById("fecha").value;
-    let hora= document.getElementById("hora");
+    let fecha = document.getElementById("fecha");
+    let hora = document.getElementById("hora");
 
-    
-    let fechaActual=new Date();
-    let year=fechaActual.getFullYear();
+
+    let fechaActual = new Date();
+    let year = fechaActual.getFullYear();
     let mes = fechaActual.getMonth();
-    let dia=fechaActual.getDay();
+    let dia = fechaActual.getDay();
 
-    let hora1=fechaActual.getDate();
-    let minutos=fechaActual.getMinutes();
-    let segundos= fechaActual.getSeconds() ;
+    let hora1 = fechaActual.getHours();
+    let minutos = fechaActual.getMinutes();
+    let segundos = fechaActual.getSeconds();
 
     console.log(mes);
+    let contarHoras = setInterval(() => {
+        fecha.textContent = `0${dia} /${mes}/ ${year}`;
+        hora.textContent = `${hora1}:${minutos}:${segundos}`;
+        segundos++;
+        if (segundos==60){
+            segundos=0;
+        };
+
+    }, 1000);
+
 
 });
