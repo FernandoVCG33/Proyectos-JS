1a2b6dc4d970260dea40b0bd1864ff5211fa5c30
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-11 15:28:38 -0400

    proyecto 21 agregado y cambios 8

--- proyecto 21/funciones.js
+++ proyecto 21/funciones.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded',  (e)=>{
     //seleccionar elementos DOM
     let inventarioDom= document.querySelector(".inventario") ;
+    let cajas=document.querySelectorAll(".inventario__caja") ;
 } );
