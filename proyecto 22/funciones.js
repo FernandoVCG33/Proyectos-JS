12964732c268c2c63adab55288effbc43b292e68
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-25 20:05:08 -0400

    agregar funcionalidad de búsqueda en el texto principal al presionar Enter

---
+++ proyecto 22/funciones.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let input = document.querySelector('.input_search');
+    let texto = document.querySelector('.main_text');
+    input.addEventListener('keydown', (e) => {
+        let search=input.value;
+        if (search.trim() != ''  && e.code=='Enter' ) {
+            let regEx= new RegExp(search, 'gi');
+            texto.innerHTML= texto.textContent.replace(regEx, "<span class='text_seacrh'>$&</span>");
+        }
+    });
+
+})
