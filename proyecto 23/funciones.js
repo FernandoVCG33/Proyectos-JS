e961a36aeb7b9ccda1d1a97b789ff8e056710c02
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2026-01-04 21:29:55 -0400

    Modificar evento de entrada de email a 'keyup' y agregar validación básica

--- proyecto 23/funciones.js
+++ proyecto 23/funciones.js
@@ -1,10 +1,23 @@
 window.addEventListener("DOMContentLoaded", () => {
     console.log('Dom Cargado al 120%');
     let inputS=document.getElementById('emails');
+    let iconoChech=document.getElementById('check');
+    let iconoError=document.getElementById('wrong');
     inputS.addEventListener('keyup', ()=>{
         let inputEmail=inputS.value;
         let parter= /@.*\./ ;
         if(inputEmail.trim() !=""){
+            let testeo= parter.test(inputEmail);
+            console.log(testeo);
+            if (testeo){
+                iconoChech.classList.remove('ocultar');
+                iconoError.classList.add('ocultar');
+            }
+            else{
+                iconoError.classList.remove('ocultar');
+            }
+
+
 
         }
 
