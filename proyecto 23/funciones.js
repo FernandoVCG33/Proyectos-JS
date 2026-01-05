d05543e82c72252c972a010266c10d2a7559527a
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2026-01-04 21:08:20 -0400

    Agregar estilos de entrada y ajustes en estilos.css e index.html

--- proyecto 23/funciones.js
+++ proyecto 23/funciones.js
@@ -1,4 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
-
     console.log('Dom Cargado al 120%');
+    let inputS=document.getElementById('emails');
+    inputS.addEventListener('keypress', ()=>{
+        let inputEmail=inputS;
+       console.log(inputEmail.value);
+    });
 });
