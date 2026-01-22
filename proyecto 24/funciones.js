02de178dc05eb79d6459bb5a37d41e41d7388f3d
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2026-01-22 18:41:57 -0400

    Agregar margen derecho y estilos para estado activo en SVG

--- proyecto 24/funciones.js
+++ proyecto 24/funciones.js
@@ -1,8 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
-    let input = document.getElementById('miInput');
+    let input = document.getElementById('input_pas');
     let icon = document.querySelector('svg');
 
     icon.addEventListener('click', () => {
+        if(input.type === 'password'  && input.value.trim() === '' ) {
 
+        }
     });
 });
